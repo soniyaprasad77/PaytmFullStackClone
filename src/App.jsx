ae5bb51be3fd125357005c2358544272c7544eec
Your Name <soniyagupta82270@gmail.com>
2024-06-23 03:41:23 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center'>
+    <h1 className='text-xl font-bold'> Paytm Clone </h1>
+    </div>
+  )
+}
+
+export default App
