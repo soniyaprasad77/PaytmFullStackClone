ecb053eadf0a69dc5d012d5d6c38bad18f095f5b
Your Name <soniyagupta82270@gmail.com>
2024-06-24 18:37:41 +0530

    add backend auth routes

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,7 +1,48 @@
 const express = require("express");
-const userRouter = require("./user")
+const userRouter = require("./user");
+const zod = require("zod");
 const router = express.Router();
+const { User } = require("../db");
 
-router.use("/user", userRouter)
+const signupSchema = zod.Schema({
+  username: zod.string(),
+  password: zod.string(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+});
+
+router.use("/user", userRouter);
+router.post("/signup", async (req, res) => {
+  const { success } = signupSchema.safeParse(req.body);
+  if (!success) {
+    res.json({
+      message: "Email already Exits / invalid Token",
+    });
+  }
+  const user = await User.findOne({
+    username: req.body.username,
+  });
+  if (user._id) {
+    res.json({
+      message: "Email already Exits / invalid Token",
+    });
+  }
+  const dbUser = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+  const token = jwt.sign(
+    {
+      userId: dbUser._id,
+    },
+    JWT_SECRET
+  );
+  res.json({
+    user: "user created successfully",
+    token:token,
+  });
+});
 
 module.exports = router;
