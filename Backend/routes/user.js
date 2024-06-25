6dc6f976907f695927e858a013b819e39d6af556
Your Name <soniyagupta82270@gmail.com>
2024-06-25 19:44:40 +0530

    add auth middleware and user routes to query in db

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,8 +1,53 @@
 const express = require("express");
 const router = express.Router();
+const zod = require("zod");
+const { authMiddleware } = require("./auth");
+const User = require("../db");
+const updateBody = zod.object({
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+  password: zod.string().optional(),
+});
+router.put("/update", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "error while updating information",
+    });
+  }
+  await User.updateOne(req.body, {
+    _id: req.body.userId,
+  });
+  res.status(200).json({
+    message: "data updated successfully",
+  });
+});
 
-router.post("/signup", () => {});
-
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
 module.exports = {
   router,
 };
