eff659bc1d4f8f83f8a3db93f953e36d408aaaaa
Your Name <soniyagupta82270@gmail.com>
2024-06-25 20:02:31 +0530

    update db.js & add Account Schema

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,54 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-mongoose.connect(process.env.MONGODB_URI);
 
-const UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/paytm")
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
 });
 
-const User = new mongoose.model("User", UserSchema);
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
-  User,
+	User,
+  Account,
 };
